ea4e65d2b035f428ac14951c8364cffff638d0ad
JeffreyAdu <104592506+JeffreyAdu@users.noreply.github.com>
2023-02-08 13:29:10 -0500

    Update questions.sql
    My answers to the exercise

--- SQL Deep Dive/Comparison Operators/questions.sql
+++ SQL Deep Dive/Comparison Operators/questions.sql
@@ -1,11 +1,13 @@
 -- How many female customers do we have from the state of Oregon (OR)?
 /*
 * Write your query here
+106
 */
 
 -- Who over the age of 44 has an income of 100 000 or more? (excluding 44)
 /*
 * Write your query here
+2497
 */
 
 -- Who between the ages of 30 and 50 has an income less than 50 000?
@@ -13,9 +15,11 @@
 
 /*
 * Write your query here
+2362
 */
 
 -- What is the average income between the ages of 20 and 50? (Excluding 20 and 50)
 /*
 * Write your query here
+59409.926240780098
 */
