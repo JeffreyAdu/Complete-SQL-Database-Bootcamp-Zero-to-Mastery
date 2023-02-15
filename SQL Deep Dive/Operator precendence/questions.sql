18cb0fc7eaa5ea054af7707f771f1530b9d41601
JeffreyAdu <104592506+JeffreyAdu@users.noreply.github.com>
2023-02-15 11:53:23 -0500

    Update questions.sql

--- SQL Deep Dive/Operator precendence/questions.sql
+++ SQL Deep Dive/Operator precendence/questions.sql
@@ -6,7 +6,14 @@
 * Select people either under 30 or over 50 with an income above 50000
 * Include people that are 50
 * that are from either Japan or Australia
+  
 */
+SELECT firstname, age, income from "public"."customers"
+WHERE (age < 30 OR age >= 50) and income > 50000
+AND (country = 'Japan' OR country = 'Australia');
+
+
+
 
 /*
 * DB: Store
@@ -14,3 +21,7 @@
 * Question: 
 * What was our total sales in June of 2004 for orders over 100 dollars?
 */
+SELECT orderdate, totalamount from "public"."orders"
+where (orderdate >= '2004-06-01' and orderdate <= '2004-06-30') and totalamount > 100;
+
+
